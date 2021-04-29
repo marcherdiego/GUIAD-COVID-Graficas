49a057c09553dd695856d0e8c6af022f7794b500
Diego Marcher <diego@ujet.co>
2021-04-28 21:52:30 -0300

    style

--- app/src/main/java/com/nerdscorner/covid/stats/ui/mvp/view/StatsView.kt
+++ app/src/main/java/com/nerdscorner/covid/stats/ui/mvp/view/StatsView.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.components.AxisBase
+import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.components.YAxis
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.formatter.ValueFormatter
@@ -16,16 +17,21 @@ abstract class StatsView(activity: AppCompatActivity) : BaseActivityView(activit
 
     fun setChartsData(dataSets: List<ILineDataSet>) {
         chart.data = LineData(dataSets)
-        chart.getAxis(YAxis.AxisDependency.RIGHT).textColor = Color.WHITE
-        chart.getAxis(YAxis.AxisDependency.LEFT).textColor = Color.WHITE
+        styleAxis(dataSets.first())
         chart.invalidate()
     }
 
     fun setChartsData(dataSet: ILineDataSet) {
         chart.data = LineData(dataSet)
+        styleAxis(dataSet)
+        chart.invalidate()
+    }
+    
+    private fun styleAxis(dataSet: ILineDataSet) {
         chart.getAxis(YAxis.AxisDependency.RIGHT).textColor = Color.WHITE
         chart.getAxis(YAxis.AxisDependency.LEFT).textColor = Color.WHITE
         chart.xAxis.apply {
+            position = XAxis.XAxisPosition.BOTH_SIDED
             textColor = Color.WHITE
             granularity = 1f
             valueFormatter = object : ValueFormatter() {
@@ -35,6 +41,5 @@ abstract class StatsView(activity: AppCompatActivity) : BaseActivityView(activit
                 }
             }
         }
-        chart.invalidate()
     }
 }
