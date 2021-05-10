b0b7f9097fe14f50704aef9bceb0a7144b878cf1
Diego Marcher <diego@ujet.co>
2021-05-10 06:35:27 -0300

    splash screen

---
+++ app/src/main/java/com/nerdscorner/guiad/stats/ui/activities/SplashActivity.kt
@@ -0,0 +1,41 @@
+package com.nerdscorner.guiad.stats.ui.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nerdscorner.guiad.stats.domain.*
+import com.nerdscorner.guiad.stats.utils.ColorUtils
+import com.nerdscorner.guiad.stats.utils.SharedPreferencesUtils
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            withContext(Dispatchers.Default) {
+                // Init shared preferences
+                SharedPreferencesUtils.init(this@SplashActivity)
+
+                // Init chart colors array
+                ColorUtils.init(this@SplashActivity)
+
+                // Load pre-saved data onto data objects
+                CitiesData.getInstance().setData(SharedPreferencesUtils.getCitiesData())
+                CtiData.getInstance().setData(SharedPreferencesUtils.getCtiData())
+                DeceasesData.getInstance().setData(SharedPreferencesUtils.getDeceasesData())
+                GeneralStatsData.getInstance().setData(SharedPreferencesUtils.getGeneralData())
+                P7ByCityData.getInstance().setData(SharedPreferencesUtils.getP7ByCityData())
+                P7Data.getInstance().setData(SharedPreferencesUtils.getP7Data())
+                MobilityData.getInstance().setData(SharedPreferencesUtils.getMobilityData())
+            }
+
+            // After finishing initializing, start MainActivity
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }
+    }
+}
